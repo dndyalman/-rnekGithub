638bdbf8e3d724af0574665332a58bd2978322fc
toshiba <toshiba@PC>
2022-04-25 15:09:15 +0300

    Add project files.

---
+++ örnekGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace örnekGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
